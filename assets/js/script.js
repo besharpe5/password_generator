770d10b43fd35e62c556468a497a551dfc4d56bf
besharpe5 <besharpe15@gmail.com>
2021-12-18 11:05:13 -0500

    set up if statements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,42 @@
 // Assignment code here
 
+function generatePassword () {
+   // console.log("this message works");
+    //return "this message works";
+    //password length 
+    var passwordLength = parseInt(window.prompt("Select required character length"));
+    if (Number.isNaN(passwordLength));
+    alert("A value needs to be entered.");
+    return null;
+    if (passwordLength < 8) {
+        alert("Password needs to be at least 8 characters in length.");
+        return null;
+    }
+    if (passwordLength > 128) {
+        alert("Password needs to be less than 128 characters in length.");
+        return null;
+    }
+    console.log(passwordLength);
+    //uppercase 
+    var isUppercaseChar = confirm("Do you want to include uppercase characters?");
+    console.log(isUppercaseChar);
+    //lowercase
+    var isLowercaseChar = confirm("Do you want to include lowercase characters?");
+    console.log(isLowercaseChar);
+    //numeric
+    var isNumericChar = confirm("Do you want to include numbers?");
+    console.log(isNumericChar);
+    //symbols
+    var isSpecialChar = confirm("Do you want to include special characters/symbols?");
+    console.log(isSpecialChar);
+
+    if (isUppercaseChar === false && isLowercaseChar === false && isNumericChar === false && isSpecialChar === false) {
+        alert("At least one option needs to be selected.");
+    }
+
+
+}
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
